56414e4636be408d5ad9adfb1d20b9c8fdbeab88
joao_amendola <joao_amendola@estudante.sesisenai.org.br>
2022-10-16 18:43:07 -0300

    Exercício 7 resolvido!

---
+++ exercicio-7/index.js
@@ -0,0 +1,44 @@
+descricao = `
+[M1S02] Ex 7 - Verifique a estação do ano
+Baseado na data atual, seu código deve verificar qual a estação atual do ano no hemisfério sul, exibindo através de um alert a estação atual.
+Considere:
+- 22/12 a 21/03 - Verão;
+- 22/03 a 21/06 - Outono;
+- 22/06 a 21/09 - Inverno;
+- 22/09 a 21/12 - Primavera.
+
+Realize o commit do exercício resolvido.
+`
+
+//Data Atual
+var dataAtual = new Date();
+var ano = dataAtual.getFullYear();
+
+//Verão
+inicioVerao = new Date(ano,12,22);
+fimVerao = new Date(ano,03,21);
+//Outono
+inicioOutono = new Date(ano,03,22);
+fimOutono = new Date(ano,06,21);
+//Inverno
+inicioInverno = new Date(ano,06,22);
+fimInverno = new Date(ano,09,21);
+//Primavera
+inicioPrimavera = new Date(ano,09,22);
+fimPrimavera = new Date(ano,12,21);
+
+if (inicioVerao <= dataAtual && dataAtual <= fimVerao){
+    alert("É verão")
+}
+
+else if (inicioOutono <= dataAtual && dataAtual <= fimOutono){
+    alert("É outono")
+}
+
+else if (inicioInverno <= dataAtual && dataAtual <= fimInverno){
+    alert("É inverno")
+}
+
+else if (inicioPrimavera <= dataAtual && dataAtual <= fimPrimavera){
+    alert("É primavera")
+}
